3d860b1aee067c5ee9658365937c9eceb2167b7a
Yeison Barrios F <yeison.barrios.funieles@gmail.com>
2024-08-06 12:47:02 -0500

    start_projects

---
+++ main.js
@@ -0,0 +1,20 @@
+// let nombre = "Camila Herrera";
+// let edad = 17;
+
+// let informacionUsuario = `Hola como ${nombre}
+// Tu edad es ${edad}`;
+
+// let estructuraHtml = 
+//     "<table>"+
+//         "<thead>"+
+
+//         "</thead>"+
+//     "</table>"
+
+
+// console.log("Hola \n"+"Bien "+edad+" "+nombre)
+
+
+
+let nombre = prompt("Ingrese su nombre").toLocaleLowerCase();
+console.log(nombre) 
